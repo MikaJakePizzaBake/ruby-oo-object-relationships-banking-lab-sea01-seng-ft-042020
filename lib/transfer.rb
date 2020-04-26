c361df73a8ede35a90d1cfa3e654b62b1d3c96d3
Trevor Low <tlow94@uw.edu>
2020-04-26 08:59:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,17 +14,18 @@ class Transfer
   def receiver
     @account
   end
-def valid?
-   @sender.valid? && @receiver.valid? ? true:false
-end
-def execute_transaction
-  if valid? && sender.balance > @amount && status == "pending"
-  sender.balance -= @amount
-  receiver.balance += @amount
-  @status = "complete"
-else
-  @status = "rejected"
-  return "Transaction rejected. Please check your account balance."
-end
-end
+  def valid?
+    @sender.valid? && @receiver.valid? ? true:false
+  end
+
+  def execute_transaction
+    if valid? && @sender.balance > @amount && status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 end
