fda78642c2e1541058e5c44db1dab9187f10690f
Trevor Low <tlow94@uw.edu>
2020-04-26 08:28:45 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,18 @@
 class BankAccount
+  attr_reader :account, :name
+  attr_accessor :balance, :status
+  def initialize (name)
+    @account = account
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  def deposit (amount)
+    self.balance += amount
+  end
+  def display_balance
+    self.balance
+    "Your balance is $#{self.balance}."
+  end
 
 end
