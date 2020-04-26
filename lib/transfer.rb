984fdc813d0c5e0fd09ecb75e3cb537c788e1e82
Trevor Low <tlow94@uw.edu>
2020-04-26 08:41:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
   # your code here
-  attr_accessor :name, :account, :balance, :status
+  attr_accessor :name, :account, :balance, :status, :amount
   def initialize (name, account, balance)
     @name = name
     @account = account
     @balance = balance
     @status = "pending"
+    @amount = amount
   end
   def sender
     @name
