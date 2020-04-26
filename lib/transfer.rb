cbb3b8ef74b5a97dee45d0f790a134835ca7c66b
Trevor Low <tlow94@uw.edu>
2020-04-26 09:06:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,4 +22,14 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
+  def reverse_transfer
+    if valid? && @receiver.balance > @amount && status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 end
