2a230fd387898a47ed6f456760b3b60bcfd9cef3
Trevor Low <tlow94@uw.edu>
2020-04-26 09:03:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,22 +1,17 @@
 class Transfer
   # your code here
-  attr_accessor :name, :account, :balance, :status, :amount
-  def initialize (name, account, balance)
-    @name = name
-    @account = account
+  attr_accessor :sender, :receiver, :balance, :status, :amount
+  def initialize (sender, receiver, balance)
+    @sender = sender
+    @receiver = receiver
     @status = "pending"
     @amount = 50
   end
-  def sender
-    @name
-  end
-  def receiver
-    @account
-  end
+
 def valid?
-  sender.status == "open" && receiver.status == "open" ? true:false
-  name.valid?
-  account.valid?
+  @sender.status == "open" && @receiver.status == "open" ? true:false
+  @sender.valid?
+  @account.valid?
 end
 def execute_transaction
   if valid? && sender.balance > @amount && status == "pending"
