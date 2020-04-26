7ed7932c68cfd04d786b5c4616e388f710a9870f
Trevor Low <tlow94@uw.edu>
2020-04-26 08:37:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
   # your code here
+  attr_accessor: :name, :account, :balance
+  def initialialize (name, account, balance)
+    @name = name
+    @account = account
+    @balance = balance
+  end
 end
